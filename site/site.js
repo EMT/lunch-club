9e96522a20cf3329320b5aaefeb8fbfa438fe24b
Harry Parton <hparton@hotmail.co.uk>
2016-01-19 16:38:34 +0000

    Clean up site.js, reverse the order of displayed items

--- site/site.js
+++ site/site.js
@@ -6,42 +6,43 @@ var _ = require('underscore');
 var app = express();
 var db = new Store("../data",{pretty:true, type:'single'});
 
-var allReviews = db.allSync();
-
-
 app.set('view engine', 'html');
-app.engine('html', require('hbs').__express);
+app.engine('html', hbs.__express);
 
 app.set('views', __dirname + '/views')
 app.use('/assets', express.static(__dirname + '/public'));
 
-app.engine('html', hbs.__express);
-
-
 app.get('/', function(req, res) {
-    res.render('index',{title:"The Lunch Club", reviews: db.allSync()});
+    res.render('index',{title:"The Lunch Club", reviews: getData()});
 });
 
-
-
 hbs.registerHelper('ratings', function() {
-  var rating = hbs.handlebars.escapeExpression(this.rating);
-
-
+  var rating = Math.round(hbs.handlebars.escapeExpression(this.rating));
   var ratingsList = '';
 
   for (var i = 0; i < 10; i++) {
-
   	if (i < rating) {
   		ratingsList += '<li><img src="/assets/triangle-filled.svg" alt=""></li>'
   	} else {
   		ratingsList += '<li><img src="/assets/triangle.svg" alt=""></li>'
   	}
-
   };
 
   return new hbs.handlebars.SafeString(ratingsList);
 });
+
+function getData(reversed) {
+	console.log('Grabbing some data from data.json');
+	var data = db.allSync();
+	var dataArrayed = Object.keys(data).map(function(k) { return data[k] });
+	var dataReversed = dataArrayed.sort().reverse();
+
+	if (reversed) {
+		return dataReversed;
+	} else {
+		return dataArrayed;
+	}
+}
 
 app.listen(5000);
 console.log('Express server listening on port 5000')
